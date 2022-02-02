5b2862acff559abded834f55d874f84a4dc9e42b
lukebreezee <breezeeluke@gmail.com>
2022-02-02 14:36:07 -0500

    First Commit

---
+++ poker.js
@@ -0,0 +1,12 @@
+const poker = (player1Cards, player2Cards) => {
+    player1Results = {};
+    player2Results = {};
+
+    for (const card of player1Cards) {
+        numOfCards = {};
+
+        if (numOfCards[card])
+    }
+}
+
+console.log(poker([1, 1, 12, 12, 7], [2, 2, 3, 4, 5]));
